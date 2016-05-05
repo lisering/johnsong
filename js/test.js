972ce378e1018e3a2a1fa372a38db0571b2404da
johnsong <johnsong@126.com>
2016-05-05 22:09:17 +0800

    add new js

---
+++ js/test.js
@@ -0,0 +1,10 @@
+var re = null,
+    i;
+for (var i = 0; i < 10; i++) {
+  var re = /cat/g;
+  re.test('catsdrafasdfa');
+}
+for (var i = 0; i < 10; i++) {
+  var re = new RegExp('cat', 'g');
+  re.test('catsdrafasdfa');
+}
