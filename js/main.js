fc4079996c623f14b28f452915efa8fe56471016
johnsong <johnsong@126.com>
2016-05-11 14:57:01 +0800

    init project

---
+++ js/main.js
@@ -0,0 +1,16 @@
+(function () {
+  
+  //variables
+  
+  var main = {
+    init: function () {
+      this.show();
+    },
+    show: function () {
+      
+    }
+  };
+  
+  main.init();
+  
+})();
